d7fb1ad8ee55ec7413be36e791ba6a0611d86e8f
uicmu <mubin41@student.sust.edu>
2022-06-03 16:09:11 +0600

    BFS

--- src/Graph_Algorithms/BGraph.java
+++ src/Graph_Algorithms/BGraph.java
@@ -1,4 +1,63 @@
 package Graph_Algorithms;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Queue;
+
 public class BGraph {
+    public int V;
+    public LinkedList<Integer> [] adjList;
+
+    public BGraph(int v) {
+        this.V = v;
+        adjList = new LinkedList[V];
+
+        for (int i = 0; i <V ; i++) {
+            adjList[i] = new LinkedList<>();
+
+        }
+    }
+    public void add_edge(int start, int end){
+        adjList[start].add(end);
+        adjList[end].add(start);
+
+    }
+
+    public void graph_printer(){
+        for (int i = 0; i <V ; i++) {
+            System.out.println("NODE ["+i+"]"+adjList[i]);
+
+        }
+    }
+    public void BFS(int start){
+        boolean visited [] = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start]=true;
+
+
+        while (!queue.isEmpty()){
+            int poped = queue.remove();
+            System.out.print(poped+"->");
+            ListIterator<Integer> l = adjList[start].listIterator();
+            for (ListIterator<Integer> it = l; it.hasNext(); ) {
+                int n = it.next();
+                if(!visited[n]){
+                    visited[n] = true;
+                    queue.add(n);
+                }
+
+            }
+            if(!queue.isEmpty()){
+                start = queue.peek();
+
+            }
+
+
+
+
+        }
+
+
+    }
 }
--- src/Testers/BGraphTester.java
+++ src/Testers/BGraphTester.java
@@ -1,4 +1,25 @@
 package Testers;
 
+import Graph_Algorithms.BGraph;
+
 public class BGraphTester {
+    public static void main(String[] args) {
+        BGraph g = new BGraph(6);
+        g.add_edge(1,0);
+        g.add_edge(0,5);
+        g.add_edge(0,2);
+        g.add_edge(1,2);
+        g.add_edge(2,5);
+        g.add_edge(1,3);
+        g.add_edge(3,4);
+        g.add_edge(4,5);
+        g.add_edge(2,4);
+
+        g.graph_printer();
+
+        System.out.print("BFS->>");
+        g.BFS(2);
+
+
+    }
 }
